651a1b4b68db7ee1535b01fa1bbc62ae8ca6d5ef
Jim <jinrimoua01@gmail.com>
2026-04-05 19:05:47 -0500

    Adjust Program.cs file

--- ZooApi/Program.cs
+++ ZooApi/Program.cs
@@ -5,6 +5,7 @@ using ZooApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get connection string: either from Render's DATABASE_URL or local appsettings.json
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 string connectionString;
 
@@ -13,7 +14,7 @@ if (!string.IsNullOrEmpty(databaseUrl))
     // Convert DATABASE_URL to Npgsql connection string
     var npgsqlBuilder = new NpgsqlConnectionStringBuilder(databaseUrl)
     {
-        SslMode = SslMode.Require
+        SslMode = SslMode.Require,
     };
     connectionString = npgsqlBuilder.ConnectionString;
 }
@@ -23,45 +24,42 @@ else
     connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 }
 
+// Add DbContext
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Add application services
 builder.Services.AddScoped<ZooService>();
 
-// Add services to the container.
-
+// Add controllers and OpenAPI
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-
+// Configure CORS for local Angular frontend
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://localhost:4200")
-                        .AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod());
+        policy => policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseCors("AllowFrontend");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Automatically apply migrations and seed database
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
